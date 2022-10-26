adc3033900e3242c89f25352bcca86b90ae206b3
Fredrik <ftillman@gmail.com>
2022-10-26 16:26:54 +0200

    Tests passing for luhn check.

--- app/src/main/java/omegapoint/LuhnCheck.java
+++ app/src/main/java/omegapoint/LuhnCheck.java
@@ -3,41 +3,30 @@ package omegapoint;
 import java.util.Optional;
 
 public class LuhnCheck implements ValidityCheck {
+
     @Override
     public Optional<String> apply(String maybePno) {
-        return Optional.empty();
-    }
-
-    public String checkSum(final String pno) {
-        final String allDigits = pno.replaceAll("[^\\d]+", "");
+        final String allDigits = maybePno.replaceAll("[^\\d]+", "");
         System.out.println("allDigits is " + allDigits);
         if (allDigits.length() < 10) {
-            return ("Too few digits");
+            return Optional.of("Too few digits");
         }
         final String lastNine = allDigits.substring(allDigits.length() - 10, allDigits.length() - 1);
         final String actualChecksum = allDigits.substring(allDigits.length() - 1);
-        System.out.println("lastNine = " + lastNine);
-        System.out.println("actualChecksum = " + actualChecksum);
         char[] digits = lastNine.toCharArray();
 
         int sum = 0;
         int multiplier = 2;
         for (char d : digits) {
-            int x = Character.getNumericValue(d);
-            sum += add(x, multiplier);
+            sum += add(Character.getNumericValue(d), multiplier);
             multiplier = 3 - multiplier;
         }
 
-        if (sum % 10 == 0) {
-            System.out.println("Checksum is zero");
-            return "";
-        } else {
-            // System.out.println("Checksum is " + sum); // XXX Ser inte rätt ut
-            // int sub = sum - (sum % 10)  + 10 - sum;
-            int sub = sum - (sum % 10) + 10 - sum;
-            System.out.println("Computed checksum " + sub);
-            return "";
-        }
+        String computedChecksum = (sum % 10 == 0) ? "" : "" + (sum - (sum % 10) + 10 - sum);
+        System.out.println("Computed checksum is " + computedChecksum);
+        return (computedChecksum.equals(actualChecksum) ?
+                Optional.empty() :
+                Optional.of("Checksum mismatch"));
     }
 
     public int add(int digit, int multiplier) {
--- app/src/test/java/omegapoint/LuhnCheckTest.java
+++ app/src/test/java/omegapoint/LuhnCheckTest.java
@@ -2,14 +2,38 @@ package omegapoint;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LuhnCheckTest {
 
     @Test
-    void apply() {
+    void validChecksum() {
         LuhnCheck luhn = new LuhnCheck();
-        String r = luhn.checkSum("811218-9876");
-        System.out.println("r is " + r);
+        Optional<String> r = luhn.apply("811218-9876");
+        assertTrue(r.isEmpty());
+    }
+
+    @Test
+    void invalidChecksum() {
+        LuhnCheck luhn = new LuhnCheck();
+        Optional<String> r = luhn.apply("811218-9879");
+        assertFalse(r.isEmpty());
+    }
+
+    @Test
+    void tooShortPno() {
+        LuhnCheck luhn = new LuhnCheck();
+        Optional<String> r = luhn.apply("811218-987");
+        assertFalse(r.isEmpty());
+    }
+
+    @Test
+    void invalidChecksum2() {
+        LuhnCheck luhn = new LuhnCheck();
+        Optional<String> r = luhn.apply("201701272394");
+        assertFalse(r.isEmpty());
     }
 }
