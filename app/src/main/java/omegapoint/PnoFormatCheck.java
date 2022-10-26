0de9dfa55684a13bc3d3ae279d32d323f1bc50bd
Fredrik <ftillman@gmail.com>
2022-10-26 18:05:35 +0200

    Seems to handle pno cases.

---
+++ app/src/main/java/omegapoint/PnoFormatCheck.java
@@ -0,0 +1,20 @@
+package omegapoint;
+
+import java.util.Optional;
+
+public class PnoFormatCheck implements ValidityCheck {
+    @Override
+    public Optional<String> apply(String maybePno) {
+        System.out.println("Checking " + maybePno);
+        // 6 + 4, possibly a plus sign
+        if (maybePno.matches("^\\d{6}[+-]?\\d{4}$")) {
+            System.out.println("Matches 6+-4");
+            return Optional.empty();
+        } else if (maybePno.matches("^(19|20|21)\\d{6}-?\\d{4}$")) {
+            System.out.println("Matches with century");
+            return Optional.empty();
+        } else {
+            return Optional.of("No match for pno format");
+        }
+    }
+}
--- app/src/main/java/omegapoint/Validator.java
+++ app/src/main/java/omegapoint/Validator.java
@@ -2,10 +2,17 @@ package omegapoint;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 class Validator {
-    private final List<ValidityCheck> validityChecks = List.of(new LuhnCheck());
+    private final List<ValidityCheck> validityChecks = List.of(new LuhnCheck(), new PnoFormatCheck());
     ValidateResponse validate(final String maybePno) {
-        return new ValidateResponse(new LinkedList<String>());
+        List<String> errors = new LinkedList<>();
+         for (ValidityCheck check: validityChecks) {
+            System.out.println("Running " + check)  ;
+            Optional<String> maybeError = check.apply(maybePno);
+             maybeError.ifPresent(errors::add);
+        }
+        return new ValidateResponse(errors);
     }
 }
